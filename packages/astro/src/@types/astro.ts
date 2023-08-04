d0b7a0776b29f7f0f9597b9e9183ff564031f1e5
bluwy <bjornlu.dev@gmail.com>
2023-08-04 22:42:13 +0800

    Update docs

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -571,12 +571,7 @@ export interface AstroUserConfig {
 	 *
 	 * When using this option, all of your static asset imports and URLs should add the base as a prefix. You can access this value via `import.meta.env.BASE_URL`.
 	 *
-	 * By default, the value of `import.meta.env.BASE_URL` includes a trailing slash. If you have the [`trailingSlash`](https://docs.astro.build/en/reference/configuration-reference/#trailingslash) option set to `'never'`, you will need to add it manually in your static asset imports and URLs.
-	 *
-	 * ```astro
-	 * <a href="/docs/about/">About</a>
-	 * <img src=`${import.meta.env.BASE_URL}image.png`>
-	 * ```
+	 * The value of `import.meta.env.BASE_URL` is the same as `base`, unless `trailingSlash: "always"` or `trailingSlash: "never"` is set, which will ensure or remove a trailing slash from the `base` respectively.
 	 */
 	base?: string;
 
